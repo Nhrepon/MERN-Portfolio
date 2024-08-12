56839f032726002d2353bf7199fde3d6aa79ee17
nhrepon <nurhossainrepon7248@gmail.com>
2024-08-12 00:00:25 -0700

    version: 1.0.0

--- src/model/LegalModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-const databaseSchema=mongoose.Schema({
-    type:{type:String, require:true, unique:true},
-    description:{type:String, require:true},
-}, {timestamps:true, versionKey:false});
-
-const LegalModel=mongoose.model('legal', databaseSchema);
-module.exports=LegalModel;
